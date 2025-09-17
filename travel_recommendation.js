3a71fe39d888caea9deb513e0cda299c053a3e14
Benhadi <dr.programme@gmail.com>
2025-09-17 22:09:39 +0100

    Adding the URLs of the imeges

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,6 +1,6 @@
 // ...existing code...
 
-fetch("./travel_recommendation.json")
+fetch(./travel_recommendation.json)
   .then((response) => {
     if (!response.ok) {
       throw new Error("Network response was not ok");
